294c48ed16edb98743b087ebcb042951b1fc3f61
zawkindev <zawkindev@gmail.com>
2024-09-30 17:15:44 +0500

    initial commit

---
+++ src/main/java/kawori/miyazono/Main.java
@@ -0,0 +1,20 @@
+package kawori.miyazono;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class Main {
+    public static void main(String[] args) {
+        StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+        Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();
+        SessionFactory factory = meta.getSessionFactoryBuilder().build();
+        Session session = factory.openSession();
+
+        factory.close();
+        session.close();
+    }
+}
